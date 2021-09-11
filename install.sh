532ed18467c9a2ef8e4f17f7058b8f6ed11b69be
Cole Reynolds <cpjreynolds@gmail.com>
2021-09-11 12:48:31 -0400

    more

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ln -s ./vimrc $HOME/.vimrc
+ln -s ./zshrc $HOME/.zshrc
+
+ln -s ./xinitrc $HOME/.xinitrc
+ln -s ./Xmodmap $HOME/.Xmodmap
+ln -s ./Xresources $HOME/.Xresources
+
+ln -s ./gitconfig $HOME/.gitconfig
+ln -s ./clang-format $HOME/.clang-format
+
+ln -s ./cpjr.zsh-theme $HOME/.oh-my-zsh/custom/themes/cpjr.zsh-theme
